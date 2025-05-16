7834cc69c9e6c74b7f51399f425b1d52c4c316ee
MariusR <mrochon@microsoft.com>
2025-05-16 10:10:32 -0700

    Changed to assistants

--- AISearch/searchSvr.py
+++ AISearch/searchSvr.py
@@ -1,5 +1,6 @@
 import os
 import json
+import time
 from mcp.server.fastmcp import FastMCP
 from openai import AzureOpenAI
 from azure.identity import DefaultAzureCredential, get_bearer_token_provider
@@ -9,7 +10,7 @@ from dotenv import load_dotenv
 
 load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), '.env'), override=True)
 
-def replace_fields_in_format(file_name, fields_values):
+def replace_fields_in_format(file_name: str, fields_values: dict) -> str:
     try:
         # Read the format string from the file
         file_path = os.path.join(os.path.dirname(__file__), "prompts", file_name)
@@ -43,7 +44,7 @@ azure_openai_api_version = os.getenv("AZURE_OPENAI_API_VERSION", "2024-05-01-pre
 # DEFINE TOOLS
 #Search web tool
 # @mcp.tool()
-def search(index_name: str, query: str) -> object:
+def search(index_name: str, query: str, justPayload: bool = False) -> object:
     """Search using Azure Search index"""
     print(f"Searching {index_name} index for: {query}")  
     # If no schema, get it from the index
@@ -57,9 +58,9 @@ def search(index_name: str, query: str) -> object:
         response = requests.get(url, headers=search_headers)
         if response.status_code == 200:
             index_schema = json.loads(response.text)
-        sortable_fields = ",".join([field["name"] for field in index_schema["fields"] if (not field["type"].startswith("Collection")) and field["sortable"]])
-        filterable_fields =",".join([field["name"] for field in index_schema["fields"] if (not field["type"].startswith("Collection")) and field["filterable"]])
-        searchable_fields = ",".join([field["name"] for field in index_schema["fields"] if (not field["type"].startswith("Collection")) and field["searchable"]])
+        sortable_fields = ", ".join([field["name"] for field in index_schema["fields"] if (not field["type"].startswith("Collection")) and field["sortable"]])
+        filterable_fields =", ".join([field["name"] for field in index_schema["fields"] if (not field["type"].startswith("Collection")) and field["filterable"]])
+        searchable_fields = ", ".join([field["name"] for field in index_schema["fields"] if (not field["type"].startswith("Collection")) and field["searchable"]])
         prompt_fields = {
             "sortable_fields": sortable_fields,
             "filterable_fields": filterable_fields,
@@ -73,52 +74,70 @@ def search(index_name: str, query: str) -> object:
             "filterDescription": filterDescription,
             "searchDescription": searchDescription
         })
-        funcDef = json.loads(funcDef_str) if funcDef_str else None        
+        funcDef = json.loads(funcDef_str) if funcDef_str else None  
+        print(json.dumps(funcDef, indent=4))
             
     token_provider = get_bearer_token_provider(credential, "https://cognitiveservices.azure.com/.default")
     client = AzureOpenAI(
         api_version=azure_openai_api_version,
         azure_endpoint=azure_openai_endpoint,
-        api_key=azure_openai_key,
-        azure_ad_token_provider=token_provider if not azure_openai_key else None
+        azure_ad_token_provider=token_provider
     )   
-
-    # Note that 'call_search' is a non-existent function used here so that OpenAI returns correctly formatted json object (otherwise, it returns a markdown string with a json embedded)
-    # and not a string. The function is not called, but the json object is returned in the response.
-    # The function is defined in the prompt and the function name is used in the tool_choice parameter. 
-    response = client.chat.completions.create(
+    assistant = client.beta.assistants.create(
         model=azure_openai_chat_deployment,
-        messages=[
-            {"role": "system", "content": "Use the following tools to answer the question."},
-            {"role": "user", "content": query}
-        ],
+        name="searchAssistant",
+        instructions="You are an AI assistant that calls the provided call_search function to search for data in Azure Search.",
         tools=[
             {
                 "type": "function",
                 "function":funcDef
             }
         ],
-        max_tokens=1000,
-        tool_choice={ "type": "function", "function": { "name": "call_search" } },
+        tool_resources={},
+        temperature=1,
+        top_p=1
     )
-    response_message = response.choices[0].message    
-    if len(response_message.tool_calls) == 1:
-        try:
-            query_options = json.loads(response_message.tool_calls[0].function.arguments)
-        except:
-            query_options = {}
+    thread = client.beta.threads.create()
+    message = client.beta.threads.messages.create(
+        thread_id=thread.id,
+        role="user",
+        content=query
+    )
+    run = client.beta.threads.runs.create(
+        thread_id=thread.id,
+        assistant_id=assistant.id
+    )
+    while run.status in ['queued', 'in_progress', 'cancelling']:
+        time.sleep(1)
+        run = client.beta.threads.runs.retrieve(
+            thread_id=thread.id,
+            run_id=run.id
+        )
+
+    if run.status == 'completed':
+        messages = client.beta.threads.messages.list(
+            thread_id=thread.id
+        )
+        print(messages)
+    elif run.status == 'requires_action':
+        query_options = json.loads(run.required_action.submit_tool_outputs.tool_calls[0].function.arguments)
+    else:
+        print(run.status)    
+
     search_payload = {
         "search": query_options.get("search"),
         "filter": query_options.get("filter"),
-        "orderby": query_options.get("orderby"),
+        "orderby": query_options.get("orderBy"),
         "top": query_options.get("top"),
         "skip": query_options.get("skip"),
         "select": query_options.get("select")
     }
+    if justPayload:
+        return search_payload
     url = f"https://{search_svc_name}.search.windows.net/indexes/{index_name}?api-version=2024-07-01"
     search_headers = {
         "Content-Type": "application/json",
-        "Authorization": "Bearer " + os.getenv("AZURE_SEARCH_ADMIN_KEY"),
+        "Authorization": "Bearer " + credential.get_token("https://search.azure.com/.default").token,
     }
     response = requests.post(url, headers=search_headers, data=json.dumps(search_payload))
     if response.status_code == 200:
@@ -140,7 +159,8 @@ def show_help() -> str:
  # execute and return the stdio output
 if __name__ == "__main__":
     # mcp.run(transport="stdio")
-    search("employees", "Who is the youngest employee in marketing?")
+    res = search("employees", "Employee in the marketing department with lowest age", True)
+    print(res)
 
 
 
